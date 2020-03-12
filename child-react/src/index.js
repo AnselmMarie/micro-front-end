91e360de32409e71506e976b79c96ef1d997f575
Anselm Marie <ansmarie@gmail.com>
2020-03-12 17:10:10 -0400

    Part 2
    • The web components are now imports into the shell app
    • starting to create the react child app

--- child-react/src/index.js
+++ child-react/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-// import 'ImperativeCounter';
-// import './web.components/DeclarativeCounter';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
